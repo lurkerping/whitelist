50e4e22aa5994760c0d34f878d745f14618220d9
lurkerping <lurkerping@gmail.com>
2018-03-04 14:18:50 +0800

    initial commit

---
+++ src/test/java/com/xplmc/learning/whitelist/WhitelistApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xplmc.learning.whitelist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhitelistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

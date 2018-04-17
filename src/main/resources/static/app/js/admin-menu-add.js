d2101223d593b4a7697c0d6f16362291a756ea76
xiaoping <lurkerping@gmail.com>
2018-04-17 20:46:30 +0800

    add admin-menu-add page frontend validation

--- src/main/resources/static/app/js/admin-menu-add.js
+++ src/main/resources/static/app/js/admin-menu-add.js
@@ -4,37 +4,39 @@
     $(function () {
         //save admin menu
         $('#admin-menu-save-button').click(function () {
-            var params = {
-                menuPid: $('#admin-menu-pid').val(),
-                menuName: $('#admin-menu-name').val(),
-                menuDesc: $('#admin-menu-desc').val(),
-                menuIconClass: $('#admin-menu-icon-class').val(),
-                menuUrl: $('#admin-menu-url').val()
-            };
-            $.ajax({
-                type: 'POST',
-                url: $('form').attr('action'),
-                data: JSON.stringify(params),
-                success: function () {
-                    alert('success');
-                    window.location.href = $('#admin-menu-undo-button').attr('href');
-                },
-                error: function (data) {
-                    if (data.responseJSON && data.responseJSON.message) {
-                        $('#admin-menu-save-button').popover({
-                            theme: 'warning',
-                            content: data.responseJSON.message
-                        });
-                    } else {
-                        $('#admin-menu-save-button').popover({
-                            theme: 'warning',
-                            content: data.status + '-' + data.statusText
-                        });
-                    }
-                },
-                contentType: 'application/json',
-                dataType: 'json'
-            });
+            if ($('#admin-menu-add-form').validator('isFormValid')) {
+                var params = {
+                    menuPid: $('#admin-menu-pid').val(),
+                    menuName: $('#admin-menu-name').val(),
+                    menuDesc: $('#admin-menu-desc').val(),
+                    menuIconClass: $('#admin-menu-icon-class').val(),
+                    menuUrl: $('#admin-menu-url').val()
+                };
+                $.ajax({
+                    type: 'POST',
+                    url: $('form').attr('action'),
+                    data: JSON.stringify(params),
+                    success: function () {
+                        alert('success');
+                        window.location.href = $('#admin-menu-undo-button').attr('href');
+                    },
+                    error: function (data) {
+                        if (data.responseJSON && data.responseJSON.message) {
+                            $('#admin-menu-save-button').popover({
+                                theme: 'warning',
+                                content: data.responseJSON.message
+                            });
+                        } else {
+                            $('#admin-menu-save-button').popover({
+                                theme: 'warning',
+                                content: data.status + '-' + data.statusText
+                            });
+                        }
+                    },
+                    contentType: 'application/json',
+                    dataType: 'json'
+                });
+            }
         });
     });
 })(jQuery);

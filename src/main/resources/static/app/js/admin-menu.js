c178b26a2d60429579cdab4460419e6a2c5f5e6a
xiaoping <lurkerping@gmail.com>
2018-04-17 21:41:06 +0800

    admin-menu delete button function

--- src/main/resources/static/app/js/admin-menu.js
+++ src/main/resources/static/app/js/admin-menu.js
@@ -2,6 +2,20 @@
     'use strict';
 
     $(function () {
+        $('.admin-menu-delete-button').click(function (e) {
+            e.preventDefault();
+            var menuId = $(this).closest('tr').children('td:first').text();
+            $.ajax({
+                type: 'DELETE',
+                url: $('form').attr('action') + '/' + menuId,
+                contentType: 'application/json',
+                dataType: 'json',
+                success: function () {
+                    alert('success');
+                    window.location.reload();
+                }
+            });
 
+        });
     });
 })(jQuery);
